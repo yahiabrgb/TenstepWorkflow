cdce26ef08dd95c1e8814e3ad0225b60b9dbfad6
yahiabrgb <mohamedyahia.bourguiba@esprit.tn>
2022-05-03 21:47:45 +0200

    First Commit

---
+++ TWF/src/main/java/TenstepWorkflow/TwfApplication.java
@@ -0,0 +1,13 @@
+package TenstepWorkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwfApplication.class, args);
+	}
+
+}
---
+++ TWF/src/test/java/TenstepWorkflow/TwfApplicationTests.java
@@ -0,0 +1,13 @@
+package TenstepWorkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
