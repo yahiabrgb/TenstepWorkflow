96e09c3defdbaf9500765d8a79455bd5c4ba633a
yahiabrgb <mohamedyahia.bourguiba@esprit.tn>
2022-07-30 14:19:58 +0200

    3rd phase

--- TWF/src/main/java/TenstepWorkflow/services/SendEmailService.java
+++ TWF/src/main/java/TenstepWorkflow/services/SendEmailService.java
@@ -20,15 +20,7 @@ public class SendEmailService {
 	public void sendEmail(String to, String topic,File file) throws MessagingException
 	{
 		
-		/*System.out.println("sending email");
-		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-		simpleMailMessage.setFrom("sonia.hadouej@esprit.tn");
-		simpleMailMessage.setTo(to);
-		simpleMailMessage.setSubject(topic);
-		 simpleMailMessage.setTex t(body,true);
-		simpleMailMessage.setSentDate(new Date());
-		javaMailSender.send(simpleMailMessage);
-		System.out.println("sent email"); */
+		
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
 		
